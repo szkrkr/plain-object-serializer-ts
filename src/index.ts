605f088dc4fa4cc782d227f8f8abc8b9949d0f75
szkrkr <s.rikuro@gmail.com>
2021-03-04 19:56:00 +0900

    fix index.ts to map files correctly

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-import serializeToPlainObject from './serializeToPlainObject';
-export default serializeToPlainObject;
-
+export { default as serializeToPlainObject} from './serializeToPlainObject'
 export { default as isISerializable} from './isISerializable';
 export { default as Serializable} from './Serializable';
 export { default as ISerializable} from './ISerializable';
